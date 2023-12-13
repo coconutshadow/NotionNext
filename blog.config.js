ba1a3fabd72c10fd5b80904610f3588fda10eb26
coconutshadow <153643893+coconutshadow@users.noreply.github.com>
2023-12-13 16:17:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'hello，world, 我是一个破画画的,偶尔写点东西,你是怎么找到这的？',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'hello，world,这里除了堆放个人杂物没有别的东西啦,你是怎么找到这的？,',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'CoconutShadow', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '想换个新的简介但是这碗米饭看起来好好吃🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '想换个新的简介但是这碗米饭看起来好好吃，事已至此先吃饭吧🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 个人网站', // 网站关键词 英文逗号隔开
 
@@ -179,7 +179,7 @@ const BLOG = {
   // 动态彩带特效
   FLUTTERINGRIBBON: process.env.NEXT_PUBLIC_FLUTTERINGRIBBON || false, // 开关
   // 静态彩带特效
-  RIBBON: process.env.NEXT_PUBLIC_RIBBON || false, // 开关
+  RIBBON: process.env.NEXT_PUBLIC_RIBBON || true, // 开关
   // 星空雨特效 黑夜模式才会生效
   STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || false, // 开关
 
